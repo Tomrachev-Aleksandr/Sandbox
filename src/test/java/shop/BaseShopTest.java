37df7f2302684daf559c13b7dbc96cf087fdac26
sasha19883 <tomrachiev@mail.ru>
2025-09-28 14:54:59 +0500

    Initial commit
    Signed-off-by: sasha19883 <tomrachiev@mail.ru>

---
+++ src/test/java/shop/BaseShopTest.java
@@ -0,0 +1,29 @@
+package shop;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import pages.ShopPage;
+import java.time.Duration;
+
+public class BaseShopTest {
+    public WebDriver browser;
+    ShopPage shopPage;
+
+    @BeforeMethod
+    public void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("--guest");
+        browser = new ChromeDriver(options);
+        browser.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        shopPage = new ShopPage(browser);
+    }
+
+    @AfterMethod
+    public void close() {
+       browser.quit();
+    }
+}
