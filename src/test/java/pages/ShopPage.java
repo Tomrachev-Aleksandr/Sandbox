83f2a0b134903a304db94129a4560e8c8eb84ae8
sasha19883 <tomrachiev@mail.ru>
2025-09-28 14:54:59 +0500

    Initial commit

---
+++ src/test/java/pages/ShopPage.java
@@ -0,0 +1,29 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ShopPage {
+    WebDriver browser;
+
+    public ShopPage(WebDriver browser) {
+        this.browser = browser;
+    }
+
+    private static final By USERNAME_INPUT = By.xpath("//input[@id='user-name']");
+    private static final By PASSWORD_INPUT = By.xpath("//input[@id='password']");
+    private static final By LOGIN_BUTTON = By.xpath("//input[@id='login-button']");
+
+    public void open() {
+        browser.get("https://www.saucedemo.com/");
+    }
+
+    public void loginWithUsername(String userName) {
+        browser.findElement(USERNAME_INPUT).sendKeys(userName); // логин
+    }
+
+    public void loginWithPassword(String password) {
+        browser.findElement(PASSWORD_INPUT).sendKeys(password);  //написать  пароль
+        browser.findElement(LOGIN_BUTTON).click(); //клик по кнопке Логин для подтверждения
+    }
+}
